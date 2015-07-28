7414673dc75b460d813c1340c447e9ee1ea59db3
Daniel Park <dpark1985@gmail.com>
2015-07-28 13:54:19 +0900

    [CORS]

--- rookie/app.js
+++ rookie/app.js
@@ -88,8 +88,12 @@ app.use(session({
 app.use(everyauth.middleware());
 app.use(router);
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
