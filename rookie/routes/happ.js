20163ff76041c156613d5973126e777c2e7e1b4b
Daniel Park <dpark1985@gmail.com>
2015-08-11 11:25:27 +0900

    [reports]

--- rookie/routes/happ.js
+++ rookie/routes/happ.js
@@ -10,22 +10,14 @@ router.get('/userlist', function (req, res, next) {
 
 
 
-router.post('/testingUserEditPW', function (req, res, next) {
-	
-	req.db.users.update({
-		login: req.body.login,
-		password: req.body.password
-	}, 
-	{
-		"$set" : 
-		{
-			password : req.body.newPW
-		}
-	}, function (err, data){
-		res.json(data);
-	});
-});
-
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	return data based on Category and its ID
+/*	Description_ko	=	카테고리와 그 ID 를 활용하여 데이터를 반환
+/*	Reference		=	templates/sports/details.html
+/*	Reference		=	DetailCtrl
+*/
 router.get('/testingDetails/:category/:id', function (req, res, next){
 	var collection = req.params.category;
 	var itemId = req.params.id;
@@ -46,15 +38,61 @@ router.get('/testingDetails/:category/:id', function (req, res, next){
 			res.json(data);
 		});
 	}
-
 });
 
+
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	Post report data based on Category and its ID
+/*	Description_ko	=	카테고리와 그 ID 를 활용하여 데이터를 반환
+/*	Reference		=	templates/sports/details.html
+/*	Reference		=	DetailCtrl
+/*	Reference		=	$scope.report()
+*/
+router.post('/testingDetails/:category/:id', function (req, res, next){
+	var collection = req.params.category;
+	var itemId = req.params.id;
+
+	if(collection == 'competitions'){
+		req.db.competitions.update({_id: req.db.ObjectId(itemId)},
+		{
+			"$set" : {
+				reports: req.body.reports
+			}
+		});
+	} else if (collection == 'courts'){
+		req.db.courts.update({_id: req.db.ObjectId(itemId)},
+		{
+			"$set" : {
+				reports: req.body.reports
+			}
+		});
+	} else if (collection == 'clubs'){
+		req.db.clubs.update({_id: req.db.ObjectId(itemId)},
+		{
+			"$set" : {
+				reports: req.body.reports
+			}
+		});
+	}
+});
+
+
+
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	Edit user password, validation done on mobile device
+/*	Description_ko	=	사용자 비밀번호 변경, 벨리데이션은 스마트폰에서 수행
+/*	Reference		=	templates/userSetting/userinfo.html
+/*	Reference		=	settingCtrl
+/*	Reference		=	$scope.openPwChangeModal()
+*/
 router.post('/testingUserEdit', function (req, res, next) {
-	
 	req.db.users.update({login: req.body.login, password: req.body.curPW}, 
 	{
-		"$set" : 
-		{
+		"$set" : {
 			password: req.body.newPW
 		}
 	}, function (err, data){
@@ -62,13 +100,17 @@ router.post('/testingUserEdit', function (req, res, next) {
 	});
 });
 
+
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	Upload user image from Mobile Device
+/*	Description_ko	=	사용자 이미지 업로드, 스마트폰
+/*	Reference		=	templates/userSetting/userinfo.html
+/*	Reference		=	settingCtrl
+/*	Reference		=	$scope.imgSetting()
+*/
 router.post('/testingUserSetting/:userID/img', function (req, res, next) {
-	//console.log('=====================');
-	//console.log('userID: ' + req.params.userID);
-	//console.log('=====================');
-	//console.log(req.files);
-
-
 	req.db.users.update({login: req.params.userID}, 
 	{
 		"$set" : 
@@ -81,18 +123,20 @@ router.post('/testingUserSetting/:userID/img', function (req, res, next) {
 		} else{
 			res.json(data);
 		}
-
-		
-
 	});
-
-
 });
 
 
-
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	Validate user ID existance
+/*	Description_ko	=	사용자 ID 존재 확인
+/*	Reference		=	templates/menu/register.html
+/*	Reference		= 	loginCtrl
+/*	Reference		=	$scope.doRegister()
+*/
 router.post('/testingUserID', function (req, res, next) {
-
 	req.db.users.findOne({
 		login: req.body.login
 	}, function (err, data){
@@ -101,6 +145,21 @@ router.post('/testingUserID', function (req, res, next) {
 });
 
 
+/*
+/*	Author 			= 	Daniel Park
+/*	Date 			= 	08/11/2015
+/*	Description_en	=	for LOGIN, REGISTER, WITHDRAW
+/*	Description_ko	=	로그인, 회원가입, 회원탈퇴 
+/*	Reference		=	templates/menu/login.html
+/*		Reference		=	loginCtrl
+/*		Reference		=	$scope.doLogin()
+/*	Reference		=	templates/menu/register.html
+/*		Reference		=	loginCtrl
+/*		Reference		=	$scope.doRegister()
+/*	Reference		= 	templates/userSetting/userinfo.html
+/*		Reference		=	settingCtrl
+/*		Reference		=	$scope.showWithdrawConfirm()
+*/
 router.post('/testing', function (req, res, next) {
 	if(req.body.status === "login"){
 		req.db.users.update({login: req.body.login}, 
@@ -112,10 +171,6 @@ router.post('/testing', function (req, res, next) {
 		}, function (err, data){
 			res.json(data);
 		});
-
-		
-
-
 	} else if(req.body.status === "register"){
 		req.db.users.insert({
 			name: req.body.name,
